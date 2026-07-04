660d79459ab5a473d1091443dc1078fbcbe1454f
kevinschacker <kevinschacker11@gmail.com>
2026-07-04 14:29:41 -0300

    Fix: Add User-Agent headers to bypass Prefectura firewall blocks

--- netlify/functions/getRiverData.js
+++ netlify/functions/getRiverData.js
@@ -3,8 +3,16 @@ const https = require('https');
 exports.handler = async function(event, context) {
   const url = 'https://contenidosweb.prefecturanaval.gob.ar/alturas/?page=historico&tiempo=7&id=532';
 
+  const options = {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3'
+    }
+  };
+
   return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
+    https.get(url, options, (res) => {
       let data = '';
 
       res.on('data', (chunk) => {
